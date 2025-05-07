5410d97102a908bf1879f5515956108b16b1102f
figoria <1056443@hr.nl>
2025-05-07 14:02:24 +0200

    added map structure

--- backend/db_connect.py
+++ backend/db_connect.py
@@ -1,17 +1,28 @@
 import psycopg2
 import os
 from dotenv import load_dotenv
+from sqlalchemy import create_engine
 
 # Laad de .env variabelen in het os.environ dictionary
 load_dotenv()
 # Maak connectie
 def db_connect():
-    conn = psycopg2.connect(
-        dbname="innovision",
-        user= os.environ.get("DB_USERNAME"),
-        password= os.environ.get("DB_PASSWORD"),
-        host="localhost",
-        port="5432"
-    )
-    return conn
+    # Fetch variables
+    USER = os.getenv("user")
+    PASSWORD = os.getenv("password")
+    HOST = os.getenv("host")
+    PORT = os.getenv("port")
+    DBNAME = os.getenv("dbname")
+ 
+    # Connect to the database
+    try:
+        engine = create_engine(f"postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}")
+        connection = engine.connect()
+        
+        return connection
+ 
+    except Exception as e:
+        print(f"Failed to connect: {e}")
+        return "Failed to connect"
+ 
 
